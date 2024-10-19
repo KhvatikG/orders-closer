367e95c8d27524d700ca941157332355e4b3bea3
Garik <khvatik.igor@gmail.com>
2024-10-19 21:01:32 +0300

    add settings

---
+++ core/settings.py
@@ -0,0 +1,15 @@
+from dotenv import load_dotenv
+
+from pydantic_settings import BaseSettings
+
+load_dotenv() # Загружаем переменные среды из .env
+
+class Settings(BaseSettings):
+
+    TOMATO_LOGIN: str
+    TOMATO_PASSWORD: str
+
+    API_URL: str = "http://smartomato.ru/api"
+
+
+SETTINGS = Settings()
