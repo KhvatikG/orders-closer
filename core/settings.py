8780fed031e8243cde9d3cb000f3b94b86a2395c
Garik <khvatik.igor@gmail.com>
2024-10-19 23:00:52 +0300

    v0.1 done

--- core/settings.py
+++ core/settings.py
@@ -9,7 +9,7 @@ class Settings(BaseSettings):
     TOMATO_LOGIN: str
     TOMATO_PASSWORD: str
 
-    COUNT_ORDERS_FOR_GET_ORDERS: int = 10
+    COUNT_ORDERS_FOR_GET_ORDERS: int = 5
 
     BASE_API_URL: str = "http://smartomato.ru/api"
 
--- main.py
+++ main.py
@@ -1,15 +1,22 @@
 from core.settings import SETTINGS
 from tomato_api.auth_method import get_tomato_auth_token
 from tomato_api.get_orders import get_orders_per_status
+from tomato_api.close_orders import close_orders
 
-
+# Получаем токен авторизации
 auth_data = get_tomato_auth_token()
 token = auth_data.get("token")
 
+# Получаем count_orders(кол-во) заказов со статусом "Подтвержден"
 count_orders = SETTINGS.COUNT_ORDERS_FOR_GET_ORDERS
 confirm_orders = get_orders_per_status(
     count = count_orders,
     status="confirm",
-    token=token)
+    token=token
+)
 
-print(confirm_orders)
+# Закрываем полученные заказы
+close_orders(
+    orders=confirm_orders,
+    token=token
+)
